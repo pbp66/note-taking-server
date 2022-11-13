ebfe35f87fa9dc21da2658d277a53c4258b254f8
James Perry <53805701+pbp66@users.noreply.github.com>
2022-11-13 15:35:37 -0500

    Reset git tracking cache

--- routes/api.js
+++ routes/api.js
@@ -1,35 +1,44 @@
 import { Router } from 'express';
 import path from 'path';
 import fs from 'fs/promises';
+import Note from "../lib/note.js";
 
 const router = Router();
-
-const dbPath = path.resolve('./db/db.json');
+const dbPath = path.resolve('./db/db.json'); 
+let notes = [];
 
 router
-    .get('/notes', (req, res) => {
-        // read db.json contents
-        fs.readFile(dbPath, 'utf-8', function (err, data) {
-            if (err) {
-                res.status(500).json(err);
-                return;
-            }
-            const json = JSON.parse(data);
-            // respond with the parsed array
-            res.json(json);
-        });
+    .get('/notes', async (req, res) => {
+        // Check if notes array is loaded. Load it if it isn't
+        if (notes.length === 0) {
+            console.log("Loadings notes from file...");
+            await fs.readFile(dbPath, 'utf-8', function (err, data) {
+                if (err) {
+                    res.status(500).json(err);
+                    return;
+                }
+                console.log(`Data: ${data}`);
+                const json = JSON.parse(data);
+                console.log(`JSON: ${json}`);
+                notes = json.map(element => {
+                    const tempNode = new Note(element.title, element.text)
+                    tempNode.setID(element.id);
+                    return tempNode;
+                });
+            });
+        }
+        console.log(notes);
+        res.json(JSON.stringify(notes));
     })
-    
-    .post('/notes', (req, res) => {
-        // Add new note to db.json
-        console.log("API Route: POST /notes");
+    .post('/notes', async (req, res) => {
+        const { title, text } = req.body;
+        notes.push(new Note(title, text));
+        await fs.writeFile(dbPath, JSON.stringify(notes), {flag: "w"});
     })
 
     .delete('/notes', (req, res) => {
         // Add new note to db.json
         console.log("API Route: DELETE /notes");
     })
-
-
 
 export default router;
