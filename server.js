72cda8747fd9071a6b51bdcabb79823ed9470dfa
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-11-13 21:30:49 -0500

    Added code to create empty database if it didn't exist

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
 import express from "express";
+import fs from 'fs/promises';
+import path from 'path';
 import routes from './routes/index.js';
+
+const dbPath = path.resolve('./db/db.json'); 
+
+// Generate db if it does not exist:
+try {
+    const fileStats = await fs.lstat(dbPath);
+} catch (err) {
+    if (err.code === "ENOENT") {
+        const { dir } = path.parse(dbPath);
+        await fs.mkdir(dir, {recursive: true});
+        await fs.open(dbPath, 'w');
+        console.log(`Created database: ${dbPath}`);
+    } else {
+        console.log(`Caught Error: ${err.code}\nFull error log:`);
+        console.log(err);
+    }
+}
 
 const app = express();
 const PORT = process.env.PORT || 3000
